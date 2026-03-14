d0bca469e5e1ce6cfb8a162cfeef4021d0535c37
Attila Sragli <attila.sragli@uniyou.hu>
2026-03-14 14:06:45 +0100

    feature: SVG representation of product De Bruijn graphs

--- lib/cellular_automata/product_de_bruijn_graph.ex
+++ lib/cellular_automata/product_de_bruijn_graph.ex
@@ -33,6 +33,104 @@ defmodule CellularAutomata.ProductDeBruijnGraph do
     end)
   end
 
+  @spec to_svg(map()) :: binary()
+  def to_svg(graph, opts \\ []) do
+    radius = Keyword.get(opts, :radius, 250)
+    center = Keyword.get(opts, :center, 300)
+    node_r = Keyword.get(opts, :center, 18)
+
+    nodes = collect_nodes(graph)
+
+    positions =
+      nodes
+      |> Enum.with_index()
+      |> Enum.map(fn {node, i} ->
+        angle = 2 * :math.pi() * i / length(nodes)
+
+        x = center + radius * :math.cos(angle)
+        y = center + radius * :math.sin(angle)
+
+        {node, {x, y}}
+      end)
+      |> Map.new()
+
+    """
+    <svg xmlns="http://www.w3.org/2000/svg" width="600" height="600">
+    <defs>
+      <marker id="arrow" markerWidth="10" markerHeight="10"
+              refX="6" refY="3"
+              orient="auto"
+              markerUnits="strokeWidth">
+        <path d="M0,0 L0,6 L9,3 z" fill="#333"/>
+      </marker>
+    </defs>
+    #{draw_edges(graph, positions)}
+    #{draw_nodes(positions, node_r)}
+    </svg>
+    """
+  end
+
+  # Collect source and target nodes
+  defp collect_nodes(graph) do
+    sources = Map.keys(graph)
+
+    targets =
+      graph
+      |> Map.values()
+      |> List.flatten()
+
+    Enum.uniq(sources ++ targets)
+  end
+
+  defp draw_edges(graph, positions) do
+    Enum.map_join(graph, "\n", fn {from, tos} ->
+      {x1, y1} = Map.fetch!(positions, from)
+
+      Enum.map_join(tos, "\n", fn to ->
+        {x2, y2} = Map.fetch!(positions, to)
+
+        """
+        <line x1="#{x1}" y1="#{y1}"
+              x2="#{x2}" y2="#{y2}"
+              stroke="#888"
+              stroke-width="1.5"
+              marker-end="url(#arrow)"/>
+        """
+      end)
+    end)
+  end
+
+  defp draw_nodes(positions, node_r) do
+    Enum.map_join(positions, "\n", fn {node, {x, y}} ->
+      label = node_label(node)
+
+      """
+      <circle cx="#{x}" cy="#{y}"
+              r="#{node_r}"
+              fill="white"
+              stroke="#333"
+              stroke-width="2"/>
+
+      <text x="#{x}" y="#{y + 4}"
+            text-anchor="middle"
+            font-family="monospace"
+            font-size="10">
+        #{label}
+      </text>
+      """
+    end)
+  end
+
+  defp node_label({a, b}) do
+    "#{tuple_bits(a)}|#{tuple_bits(b)}"
+  end
+
+  defp tuple_bits(t) when is_tuple(t) do
+    t
+    |> Tuple.to_list()
+    |> Enum.join("")
+  end
+
   defp dfs(graph, start, current, path) do
     Enum.flat_map(Map.get(graph, current, []), fn next ->
       cond do
